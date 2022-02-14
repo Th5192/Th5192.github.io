9551f9b3765423a017c4281ec171a340b995f03e
Rob <535rob@gmail.com>
2022-02-14 06:18:36 -1000

    Prep for quiz code

--- scripts/main.js
+++ scripts/main.js
@@ -35,4 +35,19 @@ myButton.onclick = function() {
     setUserName();
 }
 
+// Cat Generalship Quiz Section
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+
 
