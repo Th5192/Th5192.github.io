0f2e654c7181f042b50909adbcfea41c4d136ce9
Rob <535rob@gmail.com>
2022-02-15 08:37:35 -1000

    Created MVP calculation function

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,13 @@
+const initialValueSubmit = document.querySelector('.initialValueSubmit')
+const initialValueField = document.querySelector('.initialValueField')
+const finalValue = document.querySelector('.finalValue')
 
+initialValueSubmit.addEventListener('click', calculateCagr)
 
+function calculateCagr() {
+    const initialValue = Number(initialValueField.value)
+    let calculatedFinalValue = initialValue*(1.11 ** 20)
+    finalValue.textContent = 'In 20 years this will equal: ' + calculatedFinalValue
+}
 
 
