dba446a919e508d4ade9b550f5e6f632e5f81d54
Rob <535rob@gmail.com>
2022-02-14 19:44:18 -1000

    Began creating real css for real website

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,7 @@ myImage.onclick = function() {
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
+let mySubHeading = document.querySelector('h2');
 
 function setUserName() {
     let myName = prompt('Please enter your name.');
@@ -19,7 +20,7 @@ function setUserName() {
         setUserName()
     } else {
         localStorage.setItem('name', myName);
-        myHeading.textContent = 'Welcome cat generalissimo ' + myName;
+        //myHeading.textContent = 'Welcome cat generalissimo ' + myName;
     }
 }
 
@@ -27,7 +28,7 @@ if (!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Welcome supreme cat generalissimo ' + storedName;
+    //myHeading.textContent = 'Welcome supreme cat generalissimo ' + storedName;
 }
 
 
